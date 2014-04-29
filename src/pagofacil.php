64d9cea5965ff42a8eb643b1d13e7a969ecf8303
beco <aalcocer@gmail.com>
2014-04-28 19:27:21 -0500

    changes in order to configure PF service

--- src/pagofacil.php
+++ src/pagofacil.php
@@ -7,10 +7,9 @@ if (!defined('_PS_VERSION_'))
 class Pagofacil extends PaymentModule
 {
 
-	public function __construct()
-	{
+	public function __construct() {
         $ver = "1.0";
-        $by = "DreasmEngineering";
+        $by = "PagoFácil / DreasmEngineering";
         $this->name = 'pagofacil';
         $this->tab = 'payments_gateways';
         $this->version = $ver;
@@ -19,35 +18,145 @@ class Pagofacil extends PaymentModule
         parent::__construct();
 
         $this->displayName = $this->l('PagoFácil');
-        $this->description = $this->l('Módulo para aceptar pago de PagoFácil');
+        $this->description = $this->l('Módulo para aceptar pago con Visa, Mastercard y AMEX vía PagoFácil');
         $this->confirmUninstall = $this->l('¿Está seguro que desea borrar el módulo?');
     }
 
-    public function install()
-    {
-      if (!parent::install() || !$this->registerHook('payment'))
-         return false;
-     return true;
- }
+    public function install() {
+        if (!parent::install() || !$this->registerHook('payment')) {
+            return false;
+        }
+        return true;
+    }
 
- public function uninstall()
- {
-  if (!parent::uninstall())
-     return false;
- return true;
-}
+    public function uninstall() {
+        if (!parent::uninstall()) {
+            return false;
+        }
+        return true;
+    }
 
-public function hookPayment($params)
-{
-  if (!$this->active)
-     return;
+    public function hookPayment($params) {
+        if (!$this->active) {
+            return;
+        }
 
- $this->smarty->assign(array(
-     'this_path' => $this->_path,
-     'this_path_ssl' => Tools::getShopDomainSsl(true, true).__PS_BASE_URI__.'modules/'.$this->name.'/'
-     ));
- return $this->display(__FILE__, 'payment.tpl');
-}
+        $this->smarty->assign(array(
+                        'this_path'     => $this->_path,
+                        'this_path_ssl' => Tools::getShopDomainSsl(true, true).__PS_BASE_URI__.'modules/'.$this->name.'/'
+         ));
+        return $this->display(__FILE__, 'payment.tpl');
+    }
+    
+    public function getContent() {
+        $output = null;
+        
+        if(Tools::isSubmit('submit'.$this->name)) {
+            $pf_api_user   = strval(Tools::getValue('PF_API_USER'));
+            $pf_api_branch = strval(Tools::getValue('PF_API_BRANCH'));
+            $pf_env        = strval(Tools::getValue('PF_ENVIRONMENT'));
+            
+            //validate both apis TODO
+            if(false) {
+            
+            } else {
+                Configuration::updateValue('PF_API_USER',    $pf_api_user);
+                Configuration::updateValue('PF_API_BRANCH',  $pf_api_branch);
+                Configuration::updateValue('PF_ENVIRONMENT', $pf_env);
+                $output = "Values Saved -" . $pf_env . "-";
+            }
+        }
+        
+        return $output.$this->displayForm();
+    }
+    
+    public function displayForm() {
+    
+        $fields_form[0]['form'] = array(
+            'legend' => array(
+                'title' => 'PagoFácil configuration settings',
+            ),
+            'input' => array(
+                array(
+                    'type' => 'text',
+                    'label' => 'API Key Usuario',
+                    'name' => 'PF_API_USER',
+                    'size' => 60,
+                    'required' => true
+                ),
+                array(
+                    'type' => 'text',
+                    'label' => 'API Key Sucursal',
+                    'name' => 'PF_API_BRANCH',
+                    'size' => 60,
+                    'required' => true
+                ),
+                array(
+                    'type' => 'select',
+                    'label' => 'Environment',
+                    'name' => 'PF_ENVIRONMENT',
+                    'desc' => 'Select between Test/Production environments',
+                    'required' => true,
+                    'options' => array(
+                        'query' => array(
+                            array(
+                                'id_option' => 1,
+                                'name' => 'Staging/Tests (NON Production)',
+                            ),
+                            array(
+                                'id_option' => 2,
+                                'name' => 'Production',
+                            ),
+                        ),
+                        'id' => 'id_option',
+                        'name' => 'name',
+                    ),
+                ),
+            ),
+            'submit' => array(
+                'title' => 'Save Changes',
+                'class' => 'button',
+            )
+        );
+        
+        $helper = new HelperForm();
+        
+        $helper->module = $this;
+        $helper->name_controller = $this->name;
+        $helper->token = Tools::getAdminTokenLite('AdminModules');
+        $helper->currentIndex = AdminController::$currentIndex.'&configure='.$this->name;
+     
+        // Language
+        /*
+        $helper->default_form_language = $default_lang;
+        $helper->allow_employee_form_lang = $default_lang;
+        */
+     
+        // Title and toolbar
+        $helper->title = $this->displayName;
+        $helper->show_toolbar = true;        // false -> remove toolbar
+        $helper->toolbar_scroll = false;      // yes - > Toolbar is always visible on the top of the screen.
+        $helper->submit_action = 'submit'.$this->name;
+        $helper->toolbar_btn = array(
+            'save' =>
+            array(
+                'desc' => $this->l('Save'),
+                'href' => AdminController::$currentIndex.'&configure='.$this->name.'&save'.$this->name.
+                '&token='.Tools::getAdminTokenLite('AdminModules'),
+            ),
+            'back' => array(
+                'href' => AdminController::$currentIndex.'&token='.Tools::getAdminTokenLite('AdminModules'),
+                'desc' => $this->l('Back to list')
+            )
+        );
+        
+        
+        $helper->fields_value['PF_API_BRANCH'] = Configuration::get('PF_API_BRANCH');
+        $helper->fields_value['PF_API_USER']   = Configuration::get('PF_API_USER');
+        $helper->fields_value['PF_ENVIRONMENT']   = Configuration::get('PF_ENVIRONMENT');
+        
+        return $helper->generateForm($fields_form);
+    }
 
 	/*public function hookPaymentReturn($params)
 	{
